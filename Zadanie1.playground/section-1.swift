b4ca8f683d27faa92e2e3a999b748d43f1f3a53f
wtznc <w.tyziniec@icloud.com>
2014-12-14 22:42:23 +0100

    Dodanie pliku z kodem zrodlowym

--- Zadanie1.playground/section-1.swift
+++ Zadanie1.playground/section-1.swift
@@ -1,5 +1,36 @@
-// Playground - noun: a place where people can play
-
 import UIKit
 
-var str = "Hello, playground"
+struct Coordinate
+{
+    var latitude: Float = 0
+    var longitude: Float = 0
+}
+
+struct Address
+{
+    var street: String = ""
+    var city: String = ""
+    var country: String = ""
+}
+
+class PointOfInterest
+{
+    var coordinate = Coordinate()
+    var address = Address()
+    var name: String = ""
+    
+    init(name: String)
+    {
+        self.name = name
+    }
+    
+    func poidescription() -> String
+    {
+        return ("\(name) - (\(coordinate.latitude), \(coordinate.longitude)) - \(address.street), \(address.city), \(address.country)")
+    }
+}
+
+var punkt = PointOfInterest(name: "Kino")
+punkt.coordinate = Coordinate(latitude: 53.428544, longitude: 14.552812);
+punkt.address = Address(street: "al. Wojska Polskiego", city: "Szczecin", country: "Polska")
+punkt.poidescription()
